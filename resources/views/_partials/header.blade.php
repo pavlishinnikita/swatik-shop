2664fe09a780a73643774822b0fac816c6c7f15b
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 <pavlishin.nikita@gmail.com>
2023-04-28 22:43:03 +0300

    add config value for header link

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -8,7 +8,7 @@
 
     <div class="header__section">
         <div class="header__item headerButton">
-            <a href="#">ГруппаВК</a>
+            <a href="{{env('APP_VK_LINK')}}" target="_blank">ГруппаВК</a>
         </div>
         <div class="img__vk"><img src="/images/header/vk.png" alt=""></div>
         <div class="header__item headerButton">
